3fef6af45725b4198b0f08b5e68e85924e9c5d3c
yanknvim <yanknvim@gmail.com>
2023-09-04 14:31:43 +0900

    Add AST and stack machine

--- test.sh
+++ test.sh
@@ -12,7 +12,7 @@ assert() {
 	if [ "$actual" = "$expected" ]; then
 		echo "$input => $actual"
 	else
-		echo "$input => $expected expectet, but got $actual"
+		echo "$input => $expected expected, but got $actual"
 		exit 1
 	fi
 }
@@ -21,5 +21,8 @@ assert 0 0
 assert 42 42
 assert 21 "5+20-4"
 assert 41 " 12 + 34 - 5 "
+assert 47 "5+6*7"
+assert 15 "5*(9-6)"
+assert 4 "(3+5)/2"
 
 echo OK
